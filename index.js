4151d8b8a81e7a438fbb022a1b0c6bea5d326746
Ellis Brown <elbrown47@gmail.com>
2015-10-11 23:11:43 -0500

    update

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-var http, director, cool, bot, router, server, port;
+var http, director, bot, router, server, port;
 
 http        = require('http');
 director    = require('director');
-cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
 
 router = new director.http.Router({
   '/' : {
     post: bot.respond,
-    get: ping
   }
 });
 
@@ -26,8 +24,3 @@ server = http.createServer(function (req, res) {
 
 port = Number(process.env.PORT || 5000);
 server.listen(port);
-
-function ping() {
-  this.res.writeHead(200);
-  this.res.end("Hey, I'm Cool Guy.");
-}
